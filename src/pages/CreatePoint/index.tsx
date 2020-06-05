17626fb6580d38a5dd7d3c1e45816863d17f1e80
Fabricio Pinto Ferreira <pferreirafabricio@gmail.com>
2020-06-04 23:43:58 -0300

    get the items from api

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
-import { Map, TileLayer, Marker } from 'react-leaflet'
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from "../../services/api";
 
 import "./styles.css";
 import logo from "../../assets/logo.svg"
+import { imageOverlay } from "leaflet";
+
+interface Item {
+  id: number,
+  title: string,
+  image_url: string
+}
 
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get('/items')
+      .then(response => {
+        setItems(response.data);
+      });
+  }, []);
+
   return (
     <div id="page-create-point">
       <header>
@@ -91,30 +108,13 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/papeis-papelao.svg" alt="Teste" />
-              <span>Oléo de Cozinha</span>
-            </li>
+            {items.map(item => (
+              <li key={item.id}>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
+
           </ul>
         </fieldset>
 
